c3308a1241ab7137d2f7d1fc546881f9c0415132
Trent Monahan <sinz@d3s.co>
2013-12-09 00:00:02 +1100

    added the show command

--- NotEnoughMods_Bot.py
+++ NotEnoughMods_Bot.py
@@ -78,17 +78,60 @@ def execute(self, name, params, channel, userdata, rank):
         self.sendChatMessage(self.send, channel, "invalid command!")
         self.sendChatMessage(self.send, channel, "see =modbot help for a list of commands")
         
-def command_compile(self, name, params, channel, userdata, rank):
+def debug_compile(self, name, params, channel, userdata, rank):
     try:
         modbot.compileHTML(params[1])
     except Exception as e:
         self.sendChatMessage(self.send, channel, str(e))
         traceback.print_exc()
-def command_save(self, name, params, channel, userdata, rank):
+def debug_save(self, name, params, channel, userdata, rank):
     try:
         modbot.saveList(params[1])
     except Exception as e:
         self.sendChatMessage(self.send,channel,str(e))
+        traceback.print_exc()
+def command_show(self, name, params, channel, userdata, rank):
+    if len(params) < 2:
+        self.sendChatMessage(self.send, channel, name+ ": Insufficent amount of parameters provided.")
+        self.sendChatMessage(self.send, channel, name+ ": "+help["list"][1])
+        return
+    if len(params) >= 3:
+        version = params[2]
+    else:
+        version = modbot.current
+    try:
+        data = modbot.lists[version]
+        if params[1] not in data:
+            #TODO: search aliases here
+            self.sendChatMessage(self.send, channel, "Mod not found.")
+            return
+       
+        darkgreen = "03"
+        red = "05"
+        purple = "06"
+        orange = "7"
+        blue = "12"
+        pink = "13"
+        gray = "14"
+        bold = unichr(2)
+        colour = unichr(3)
+        alias = colour
+        if data[params[1]]["aliases"] != "":
+            alias = colour+"("+colour+pink+str(re.sub(" ", colour+', '+colour+pink, data[params[1]]["aliases"]))+colour+") "
+        comment = colour
+        if data[params[1]]["comment"] != "":
+            comment = str(colour+"("+colour+gray+data[params[1]]["comment"]+colour+") ")
+        dev = colour
+        try:
+            if data[params[1]]["dev"] != "":
+                dev = str(colour+" ("+colour+gray+"dev"+colour+": "+colour+red+data[params[1]]["dev"]+colour+")")
+        except Exception as error:
+            print(error)
+            traceback.print_exc()
+            #lol
+        self.sendChatMessage(self.send, channel, colour+purple+params[1]+" "+alias+colour+darkgreen+data[params[1]]["version"]+dev+" "+comment+colour+orange+data[params[1]]["shorturl"]+colour)  
+    except Exception as error:
+        self.sendChatMessage(self.send, channel, name+": "+str(error))
         traceback.print_exc()
         
 rankTranslate = {
@@ -99,13 +142,43 @@ rankTranslate = {
 }
 commands = {
     "compile" : {
-        "function" : command_compile,
+        "function" : debug_compile,
         "rank" : 2,
-        "help" : ["<version>", "Debug command for generating the HTML pages"]
+        "help" : "Debug command for generating the HTML pages",
+        "args" : [
+            {
+                "name" : "version",
+                "description" : "The MC Version to save the html page for.",
+                "required" : True
+            }
+        ]
     },
     "save" : {
-        "function" : command_save,
+        "function" : debug_save,
         "rank" : 2,
-        "help" : ["<version>", "Debug command for generating the JSON files"]
+        "help" : "Debug command for generating the JSON files",
+        "args" : [
+            {
+                "name" : "version",
+                "description" : "The MC Version to save the JSON file for.",
+                "required" : True
+            }
+        ]
+    },
+    "show" : {
+        "function" : command_show,
+        "rank" : 0,
+        "help" : "Does a direct search for the mod, then outputs info",
+        "args" : [
+            {
+                "name" : "mod name",
+                "description" : "The mod to output info of",
+                "required" : True
+            }, {
+                "name" : "version",
+                "description" : "The MC version to search in.",
+                "required" : False
+            }
+        ]
     }
 }
